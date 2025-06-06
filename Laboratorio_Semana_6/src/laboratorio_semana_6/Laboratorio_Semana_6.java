3041ee2fb356e05a6d767154e63bf2d6b6da2754
EstebanFerS <151755309+EstebanFerS@users.noreply.github.com>
2025-06-06 08:23:04 -0600

    Initial Commit

---
+++ Laboratorio_Semana_6/src/laboratorio_semana_6/Laboratorio_Semana_6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio_semana_6;
+
+/**
+ *
+ * @author esteb
+ */
+public class Laboratorio_Semana_6 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
